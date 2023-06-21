2633acc3c39cab62d7b0f2ea9f2daefc4789a074
Ivan Poplayhin <poplayhin2002@gmail.com>
2023-06-22 00:38:30 +0500

    v4

--- main.py
+++ main.py
@@ -2,15 +2,11 @@ from telebot import types
 from telebot.async_telebot import AsyncTeleBot
 import asyncio
 import os
-
-from telebot.asyncio_filters import SimpleCustomFilter
-from telebot.types import ForceReply
-from telebot.types import ReplyKeyboardMarkup, KeyboardButton
-from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
+import json
 
 
 bot = AsyncTeleBot('6286035570:AAGUyK_aRQaZdERTc3ZDYB5fqY-mcH6UWQM')
-bd_file_name = 'bd.txt'
+bd_file_name = 'bd_candidate.json'
 
 questions_list = [['Укажите ваше ФИО', None],
                   ['Возвраст (число полных лет)', None],
@@ -22,36 +18,69 @@ questions_list = [['Укажите ваше ФИО', None],
                                                         'не оконченое среднее': 'not_middle'}],
                   ['Название учебного заведения', None]]
 
-index_question = 0
 
 def first_power():
+    if os.path.isfile('session.json'):
+        print("Файл существует")
+    else:
+        session = {}
+        with open('session.json', 'w') as file:
+            # Записываем данные в JSON-файл
+            json.dump(session, file)
+
     if os.path.isfile(bd_file_name):
         print("Файл существует")
     else:
-        with open(bd_file_name, 'w', encoding= 'UTF-8') as file:
-            print('Файл создан')
+        bd_candidate = {}
+        with open(bd_file_name, 'w') as file:
+            # Записываем данные в JSON-файл
+            json.dump(bd_candidate, file)
+
+
 
 def new_user_reg(message):
     chat_id = message.from_user.id
+    print(type(chat_id))
     new_user_bool = False
-    with open(bd_file_name, 'r', encoding= 'UTF-8') as file:
-        file_bd = file.read()
-        if len(file_bd) != 0:
-            if str(chat_id) in file_bd:
-                print("Пользователь уже зарегистрирован")
-                new_user_bool = False
-            else:
-                print("Пользователь не зарегистрирован")
-                new_user_bool = True
-        else:
-            print("Пользователь не зарегистрирован")
-            new_user_bool = True
-    if new_user_bool:
-        with open(bd_file_name, 'a', encoding= 'UTF-8') as file:
-            file.write(str(chat_id) + '|'+ str(message.from_user.first_name) + '\n')
+    with open('session.json', 'r') as file:
+        # Загружаем данные из JSON-файла
+        session = json.load(file)
+    if str(chat_id) in session:
+        print("Пользователь уже зарегистрирован")
+        return True
+    else:
+        print("Пользователь не зарегистрирован")
+        session[chat_id] = {
+            'name': message.from_user.first_name,
+            'index_question': 0
+        }
+        with open('session.json', 'w') as file:
+            json.dump(session, file, ensure_ascii=False)
         return False
 
-    return True
+def get_session():
+    with open('session.json', 'r') as file:
+        # Загружаем данные из JSON-файла
+        session = json.load(file)
+    return session
+def save_session(session):
+    with open('session.json', 'w') as file:
+        # Записываем данные в JSON-файл
+        json.dump(session, file, ensure_ascii=False)
+
+async def response_handler(chat_id, session):
+    global bot_message
+    index_question = session[str(chat_id)]['index_question']
+    if questions_list[index_question][1] == None:
+        markup = types.ForceReply(selective=False)
+        await bot.delete_message(chat_id, bot_message.id)
+        bot_message = await bot.send_message(chat_id, questions_list[index_question][0], reply_markup=markup)
+
+    else:
+        await bot.delete_message(chat_id, bot_message.id)
+        bot_message = await bot.send_message(chat_id, questions_list[index_question][0], reply_markup=create_keyboard_markup(questions_list[index_question][1]))
+
+
 
 
 def create_keyboard_markup(button_dict):
@@ -64,7 +93,6 @@ def create_keyboard_markup(button_dict):
 
 @bot.message_handler(commands=['help', 'start'])
 async def send_welcome(message):
-    global chat_id
     chat_id = message.from_user.id
     print(str(chat_id) + " " + str(message.from_user.first_name))
     button_dict = {
@@ -75,19 +103,16 @@ async def send_welcome(message):
     global bot_message
     if new_user_reg(message):
         bot_message = await bot.send_message(chat_id, 'С возвращением, ' + str(message.from_user.first_name) + '!', reply_markup=keyboard)
-        print(bot_message.id)
     else:
         bot_message = await bot.send_message(chat_id, 'Привет, рад познокомится, ' + str(message.from_user.first_name) + '!', reply_markup=keyboard)
 
 
 @bot.callback_query_handler(func=lambda call: True)
 async def handle_callback(call):
-    global index_question
     global bot_message
-    #global chat_id
     chat_id = call.message.chat.id
     button_call = call.data
-    # Обработка выбранной кнопки
+    session = get_session()
     if button_call == 'about_as':
         await bot.edit_message_text("Вы выбрали кнопку 1.", chat_id, bot_message.id)
     elif button_call == 'record_in_PD':
@@ -99,15 +124,9 @@ async def handle_callback(call):
     elif button_call =='start_resume':
         global start_resume
         start_resume = call
-        if questions_list[index_question][1] == None:
-            markup = types.ForceReply(selective=False)
-            await bot.delete_message(chat_id, bot_message.id)
-            bot_message = await bot.send_message(chat_id, questions_list[index_question][0], reply_markup=markup)
-            #index_question += 1
-        else:
-            await bot.delete_message(chat_id, bot_message.id)
-            bot_message = await bot.send_message(chat_id, questions_list[index_question][0],  reply_markup=create_keyboard_markup(questions_list[index_question][1]))
-            #index_question += 1
+        await response_handler(chat_id, session)
+    elif button_call =='high':
+        pass
     elif button_call == 'main':
         button_dict = {
             'О нас': 'about_as',
@@ -118,10 +137,11 @@ async def handle_callback(call):
 
 @bot.message_handler(func=lambda message: True)
 async def handle_reply(message):
-    print(message.reply_to_message)
+    chat_id = message.chat.id
     if message.reply_to_message is not None:
-        global index_question
-        index_question += 1
+        session = get_session()
+        session[str(chat_id)]['index_question'] += 1
+        save_session(session)
         await handle_callback(start_resume)
         await bot.delete_message(chat_id, message.id)
 
